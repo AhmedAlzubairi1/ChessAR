2439f25638f9e0cdbc285def56be2aaee097d1f7
OwlPudding <acortesose@gmail.com>
2021-04-03 13:45:38 -0400

    Piece movement (square selection + transform method)

---
+++ ChessAR/Assets/Scripts/CurrentSquare.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentSquare : MonoBehaviour {
+    public GameObject currentSquare;
+    void OnTriggerStay(Collider other) {
+        // Detect the square a piece is on
+        if (other.gameObject.tag == "Square") {
+            if (currentSquare != other.gameObject) {
+                // only update the public variable when it's a new square
+                currentSquare = other.gameObject;
+            }
+        }
+    }
+}
--- ChessAR/Assets/Scripts/RaySelect.cs
+++ ChessAR/Assets/Scripts/RaySelect.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class RaySelect : MonoBehaviour {
   public Camera cm;
-  public Color selectColor;
-  public GameObject selected;
-  private Color oldColor;
-  private GameObject prevGm;
-  private Renderer prevGmr;
+  public Color pieceSelectColor;
+  public Color squareSelectColor;
+  public GameObject selectedPiece;
+  public GameObject selectedSquare;
+  public GameObject moveButton;
 
+  [Range(0f, 4f)]
+  public float movementSpeed = 3f;
+
+  private Color oldColorPiece;
+  private GameObject prevGmPiece;
+  private Renderer prevGmrPiece;
+  private GameObject selectedPieceSquare;
+  private Color oldColorSquare;
+  private GameObject prevGmSquare;
+  private Renderer prevGmrSquare;
+  private int journeyLength = 2;
+
+  void Start() {
+    moveButton.SetActive(false);
+  }
   void Update() {
   #if UNITY_IPHONE || UNITY_ANDROID
     if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)) {
@@ -20,8 +35,7 @@ public class RaySelect : MonoBehaviour {
       FindObj();
     }
   #endif
-
-    // SelectedHandler();
+    SelectedHandler();
   }
   void FindObj() {
     // Cast ray from the screen
@@ -29,29 +43,78 @@ public class RaySelect : MonoBehaviour {
     Ray raycast = cm.ScreenPointToRay(Input.mousePosition);
 
     // Detect raycast hit an object
-    if (Physics.Raycast(raycast, out hit)) {
-      GameObject gm = hit.transform.gameObject;
-      if (gm.tag == "Piece") { // make sure it's a piece
-        Renderer gmr = gm.GetComponent<Renderer>(); // store current gameObject's renderer
-        bool same = false;
-        if (prevGm && prevGmr) { // if a previous selection exists
-          prevGmr.material.color = oldColor; // set it to old color
-          if (prevGm == gm) { same = true; } // same as previous selection...
-        }
-        prevGm = gm; // overwrite previous selection gameObject with current
-        prevGmr = gmr; // overwrite previous selection renderer with current
+    if (!moveButton.activeSelf) {
+      if (Physics.Raycast(raycast, out hit)) {
+        GameObject gm = hit.transform.gameObject;
+        if (gm.tag == "Piece") { // make sure it's a piece
+          Renderer gmr = gm.GetComponent<Renderer>(); // store current gameObject's renderer
+          bool same = false;
+          if (prevGmPiece && prevGmrPiece) { // if a previous selection exists
+            prevGmrPiece.material.color = oldColorPiece; // set it to old color
+            if (prevGmPiece == gm) { same = true; } // same as previous selection...
+          }
+          prevGmPiece = gm; // overwrite previous selection gameObject with current
+          prevGmrPiece = gmr; // overwrite previous selection renderer with current
 
-        oldColor = gmr.material.color; // store current selection's color
-        if (!same) { // only indicate new selection for different pieces
-          gmr.material.color = selectColor;
-          selected = gm;
-        } else {
-          selected = null;
+          oldColorPiece = gmr.material.color; // store current selection's color
+          if (!same) { // only indicate new selection for different pieces
+            gmr.material.color = pieceSelectColor;
+            selectedPiece = gm;
+            selectedPieceSquare = gm.GetComponent<CurrentSquare>().currentSquare;
+          } else {
+            selectedPiece = null;
+          }
+        } else if (gm.tag == "Square") { // make sure it's a square, all same as above
+          Renderer gmr = gm.GetComponent<Renderer>(); // store current gameObject's renderer
+          bool same = false;
+          if (prevGmSquare && prevGmrSquare) { // if a previous selection exists
+            prevGmrSquare.material.color = oldColorSquare; // set it to old color
+            if (prevGmSquare == gm) { same = true; } // same as previous selection...
+          }
+          prevGmSquare = gm; // overwrite previous selection gameObject with current
+          prevGmrSquare = gmr; // overwrite previous selection renderer with current
+
+          oldColorSquare = gmr.material.color; // store current selection's color
+          if (!same) { // only indicate new selection for different pieces
+            gmr.material.color = squareSelectColor;
+            selectedSquare = gm;
+          } else {
+            selectedSquare = null;
+          }
         }
       }
     }
   }
-  // void SelectedHandler() {
-  //   selected.
-  // }
+  void SelectedHandler() {
+    if (selectedPiece && selectedSquare) {
+      moveButton.SetActive(true);
+    } else {
+      moveButton.SetActive(false);
+    }
+  }
+  public void MovePiece() {
+    StartCoroutine(TranslatePiece());
+  }
+  private IEnumerator TranslatePiece() {
+    for (float t = 0; t < journeyLength; t += Time.deltaTime) {
+      float alpha = t / journeyLength;
+      selectedPiece.transform.position = Vector3.Lerp(
+        selectedPiece.transform.position,
+        selectedSquare.transform.position,
+        alpha
+      );
+      yield return null;
+    }
+    selectedPiece.transform.position = selectedSquare.transform.position;
+    moveButton.SetActive(false);
+    prevGmrPiece.material.color = oldColorPiece;
+    prevGmrSquare.material.color = oldColorSquare;
+    selectedPiece = null;
+    selectedSquare = null;
+    prevGmPiece = null;
+    prevGmrPiece = null;
+    selectedPieceSquare = null;
+    prevGmSquare = null;
+    prevGmrSquare = null;
+  }
 }
