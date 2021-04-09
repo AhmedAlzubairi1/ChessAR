1977fdb4f06afb17975e03ae7fc81baa37ef171b
A <a@As-MacBook-Pro.local>
2021-04-10 02:11:28 +0800

    finished valid moves. TODO: Capture and Castling

---
+++ ChessAR/Assets/Scripts/CurrentPiece.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentPiece : MonoBehaviour
+{
+    public GameObject currentPiece;
+    // list of tag of pieces
+    private List<string> pieces_list = new List<string>() {
+    "Rook",
+    "Bishop",
+    "Knight",
+    "Queen",
+    "King",
+    "Pawn"
+    };
+    void OnTriggerStay(Collider other) 
+    {
+        // Detect the square a piece is on
+        if (pieces_list.Contains(other.gameObject.tag)) 
+        {
+            // only update the public variable when it's a new square
+            currentPiece = other.gameObject;
+        }
+    }
+    // set the currentPiece to be null
+    void OnTriggerExit(Collider other) 
+    {
+        currentPiece = null;
+    }
+}
