0db7f22a0f7b2923aae8b666bab3c0b03e2ecc8c
guosy1998 <32640170+guosy1998@users.noreply.github.com>
2021-04-15 03:29:46 +0800

    Update CurrentSquare.cs
    Add position settings when start

--- ChessAR/Assets/Scripts/CurrentSquare.cs
+++ ChessAR/Assets/Scripts/CurrentSquare.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurrentSquare : MonoBehaviour {
+public class CurrentSquare : MonoBehaviour
+{
     public GameObject currentSquare;
-    void OnTriggerStay(Collider other) {
+
+    void Start()
+    {
+        Vector3 position = this.transform.localPosition;
+        position.y = RaySelect.initialHeight;
+        this.transform.localPosition = position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
         // Detect the square a piece is on
         if (other.gameObject.tag == "Square") {
             if (currentSquare != other.gameObject) {
